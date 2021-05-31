52382e0222e54c5b1216659c62e329ebf452af85
WuXiangGuJun <3344207732@qq.com>
2021-05-31 17:25:49 +0800

    新建项目，并且添加log日志

---
+++ SGame/app/src/main/java/com/wuxianggujun/sgame/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.wuxianggujun.sgame.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.wuxianggujun.sgame.R;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		setContentView(R.layout.activity_main);
+		super.onCreate(savedInstanceState);
+		
+	}
+
+}
